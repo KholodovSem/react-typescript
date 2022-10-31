183938b25d9e83002f38c01a414b3d34604641ae
Sem Kholodov <kholodovsem@gmail.com>
2022-10-31 23:18:33 +0200

    initial project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Product } from "./components/Product";
+import { Spinner } from "./components/Spinner";
+import { Error } from "./components/Error";
+import { useFetchProducts } from "./hooks/fetchProducts";
+import { Modal } from "./components/Modal";
+import { CreateProduct } from "./components/CreateProduct";
 
 function App() {
+  const { products, isLoading, error, addNewProduct } = useFetchProducts();
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto max-w-2xl pt-5">
+      <button
+        className="py-2 px-4 border bg-yellow-400 hover:text-white"
+        onClick={() => setShowModal(!showModal)}
+      >
+        Create Product
+      </button>
+      {products &&
+        products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      {isLoading && <Spinner />}
+      {error && <Error message={error} />}
+      {showModal && (
+        <Modal>
+          <CreateProduct
+            setShowModal={setShowModal}
+            addNewProduct={addNewProduct}
+          />
+        </Modal>
+      )}
     </div>
   );
 }
